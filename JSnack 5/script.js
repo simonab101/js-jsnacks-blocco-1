406f82c3d93662e83014c25e3d6d7f6046ede309
simonab101 <simo.buscicchio@gmail.com>
2023-03-28 17:25:25 +0200

    {add] script

---
+++ JSnack 5/script.js
@@ -0,0 +1,13 @@
+let array = []
+
+for(let i = 0 ; i < 6; i++){
+  let number = parseInt(prompt('Inserisci un numero:'));
+  
+  if(number % 2 !==0);
+  array.push(number);{
+
+  let output= document.getElementById('output').innerHTML= `Numeri dispari: + ${array.push(number)}`;
+  } 
+}
+console.log(array)
+
